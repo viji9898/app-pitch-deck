42e96b68caca4a21391ebc90efd20702a3bf1e82
viji9898 <vijitha.wijesuriya@gmail.com>
2023-08-28 01:19:02 +0530

    update-hero-image

--- src/components/pitchDeck/pitchHero.js
+++ src/components/pitchDeck/pitchHero.js
@@ -5,15 +5,49 @@ export const PitchHero = () => {
     <Row
       gutter={[18, 18]}
       style={{
-        maxWidth: "1050px",
+        borderRadius: "5px",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "rgb(225, 225, 219)",
+        paddingTop: "15px",
+        textAlign: "center",
+        backgroundImage: `url(https://customer-apps-techhq.s3.eu-west-2.amazonaws.com/app-pitch-deck/background_hero_pitch_deck-01.png)`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        width: "100%",
+        minHeight: "40vh",
+        maxWidth: "1200px",
+        margin: "0px auto",
         textAlign: "center",
         paddingTop: "50px",
       }}
     >
       <Col span={24}>
-        <br></br>
-        <h1>{"Grow faster"}</h1>
-        <h3>{"Ideation. Seeding. Get Funded"}</h3>
+        <span>
+          <span
+            style={{
+              background: "black",
+              padding: "5px",
+              fontSize: "30px",
+              color: "white",
+            }}
+          >
+            {"Grow faster"}
+          </span>
+          <br></br>
+          <br></br>
+          <span
+            style={{
+              background: "black",
+              padding: "5px",
+              fontSize: "40px",
+              color: "white",
+            }}
+          >
+            {"Ideation. Seeding. Get Funded"}
+          </span>
+        </span>
       </Col>
       {/* <Col xs={24} sm={24} md={24}>
         <Button
@@ -25,34 +59,32 @@ export const PitchHero = () => {
           {"Apply"}
         </Button>
       </Col> */}
-      <Divider />
-      <Col span={24}>
-        <Card
-          style={{ backgroundColor: "#f4f1ed" }}
-          title={
-            <div style={{ marginTop: "10px", textAlign: "center" }}>
-              <Col span={24}>
-                <h2>{"We Help You Build Your Perfect Pitch!"}</h2>
-              </Col>
-            </div>
-          }
-        >
-          <div style={{ textAlign: "center" }}>
-            <Button
-              href="https://pitch.techforgood.lk"
-              target="_blank"
-              rel="nonferrer"
-              size="large"
-              shape="round"
-            >
-              <SelectOutlined style={{ verticalAlign: "baseline" }} />
-              {"https://pitch.techforgood.lk"}
-            </Button>
-            <br></br>
-            <small>Check out this live pitch</small>
+
+      <Card
+        style={{ backgroundColor: "white", boxShadow: " 10px 10px 1px #000" }}
+        title={
+          <div style={{ marginTop: "10px", textAlign: "center" }}>
+            <Col span={24}>
+              <h2>{"We Help You Build Your Perfect Pitch!"}</h2>
+            </Col>
           </div>
-        </Card>
-      </Col>
+        }
+      >
+        <div style={{ textAlign: "center" }}>
+          <Button
+            href="https://pitch.techforgood.lk"
+            target="_blank"
+            rel="nonferrer"
+            size="large"
+            shape="round"
+          >
+            <SelectOutlined style={{ verticalAlign: "baseline" }} />
+            {"https://pitch.techforgood.lk"}
+          </Button>
+          <br></br>
+          <small>Check out this live pitch</small>
+        </div>
+      </Card>
     </Row>
   );
 };
